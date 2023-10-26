a5cb1ca58f908f2381801f3d543518b4a4b6389f
BoyuanLu <blu38@wisc.edu>
2023-10-26 17:37:09 -0500

    fix main function bug add submodule for gtest

--- main.cpp
+++ main.cpp
@@ -8,21 +8,14 @@ int main(int argc, char **argv) {
     std::cerr << "Please input the target path!\n";
   }
 
-  // input folder
-  if (argc == 1) {
-    Path input_folder{argv[0]};
-    Path output_folder{argv[1]};
-    digital_shoreline_analysis_system(input_folder, output_folder);
-  }
-
   // input paths
   if (argc > 1) {
-    std::vector<Path> input_paths(argc - 1);
-    for (int i = 0; i < argc - 1; i++) {
+    std::vector<Path> input_paths;
+    for (int i = 1; i < argc - 1; i++) {
       input_paths.emplace_back(argv[i]);
     }
     Path output_path{argv[argc - 1]};
-    digital_shoreline_analysis_system(input_paths, output_path);
+    dsas::dsas(input_paths, output_path);
   }
 
   return 0;
