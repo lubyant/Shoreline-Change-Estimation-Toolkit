552072728d717f7f22b36060f460d7dfef772853
BoyuanLu <blu38@wisc.edu>
2023-12-03 00:08:08 -0600

    Add a test for image class

--- src/test/test_image.cpp
+++ src/test/test_image.cpp
@@ -5,14 +5,14 @@
 #include <filesystem>
 
 #include "../dsas/image.h"
-#include "../dsas/utility.h"
 #include "gtest/gtest.h"
 
 TEST(TestImage, TestLoad) {
   using namespace std;
   filesystem::path image_path{
-      "/home/lby/Desktop/DSAS_cpp/img/final_raster/4108603/4108603_2018.tif"};
+      "/home/lby/Desktop/shorecalculator/img/final_raster/4108603/"
+      "4108603_2018.tif"};
   auto *img = new dsas::Image(image_path);
-  EXPECT_EQ(img->shorelines_.size(), 1);
+  EXPECT_EQ(img->year_, 2018);
   delete img;
 }
