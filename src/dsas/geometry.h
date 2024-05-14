6112f15f57a057b51570fcaef6cea7cd31f757d3
lubyant <blu38@wisc.edu>
2024-05-13 22:58:22 -0500

    Move some alias into geometry.h(#2)

--- src/dsas/geometry.h
+++ src/dsas/geometry.h
@@ -11,6 +11,7 @@
 #include <ogr_core.h>
 
 #include <cmath>
+#include <filesystem>
 #include <iostream>
 #include <optional>
 #include <tuple>
@@ -122,7 +123,7 @@ struct BaselineSeg : public LineSegment {
   }
 };
 
-#define transect_t int, int, int, double, int, const char*
+#define transect_t int, int, int, double, int, const char *
 struct TransectLine : public LineSegment,
                       MultiLine<Point<double>>,
                       GDALShpSaver<transect_t> {
@@ -180,8 +181,8 @@ struct TransectLine : public LineSegment,
   }
 
   [[nodiscard]] std::vector<std::string> get_names() const override {
-    return {"TransectId", "BaselineId",   "ImageId",
-            "ChangeRate", "Nums", "Info"};
+    return {"TransectId", "BaselineId", "ImageId",
+            "ChangeRate", "Nums",       "Info"};
   }
   [[nodiscard]] std::vector<OGRFieldType> get_types() const override {
     return {OGRFieldType::OFTInteger, OGRFieldType::OFTInteger,
@@ -295,6 +296,15 @@ struct IntersectPoint : public Point<double>,
   }
 };
 
+struct Transects {
+  int baseline_id_;
+  std::vector<TransectLine> transects_;
+};
+
+using Path = std::filesystem::path;
+using Baselines = std::vector<Baseline>;
+
+using TransectGroups = std::vector<Transects>;
 }  // namespace gm
 
 #endif  // DSAS_CPP_GEOMETRY_H
