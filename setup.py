bb969407d4311fd912b351555847f1704200307c
Lubyant <luboyuan1994@gmail.com>
2024-06-12 12:59:22 -0500

    wip

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import os
-import re
 import sys
-import platform
 import subprocess
-
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
@@ -17,47 +14,44 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            subprocess.check_output(['cmake', '--version'])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions.")
 
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        extdir = os.path.abspath(os.path.dirname(
+            self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
-
+        # Setup environment variables for Conda
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                             self.distribution.get_version())
+        env['CMAKE_PREFIX_PATH'] = os.environ.get(
+            'CONDA_PREFIX', "")  # Ensures CMake uses Conda's path
+
+        cfg = 'Debug' if self.debug else 'Release'
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
+
+        subprocess.check_call(['cmake', ext.sourcedir] +
+                              cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(
+            ['cmake', '--build', '.', '--target', ext.name], cwd=self.build_temp)
+
 
 setup(
     name='cppext',
     version='0.1',
     author='Your Name',
-    description='A test module package with CMake extension',
+    description='A C++ extension for Python',
     long_description='',
-    ext_modules=[CMakeExtension('cppext')],
-    cmdclass=dict(build_ext=CMakeBuild),
+    ext_modules=[CMakeExtension('cppext', 'src')],
+    cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
 )
- 
