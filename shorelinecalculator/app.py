6abcb518f666e18622369212bfc9bcb90a12cef1
lubyant <blu38@wisc.edu>
2024-06-13 21:46:53 -0500

    wip

--- shorelinecalculator/app.py
+++ shorelinecalculator/app.py
@@ -1,2 +1,2 @@
-def hello():
-    print("hello")
+import cppext
+cppext.cppext("a", "b")
--- shorelinecalculator/pybind.cpp
+++ shorelinecalculator/pybind.cpp
@@ -5,8 +5,10 @@ namespace py = pybind11;
 
 void cppext(const std::string &input_folders, const std::string &output_folder);
 void cppext(const std::string &input_folders, const std::string &output_folder){
+  std::cout << "cpp extenstion!\n";
 }
 
 PYBIND11_MODULE(cppext, m){
-
+  m.doc() = "cpp extension for calculate the shoreline erosion";
+  m.def("cppext", &cppext, "function take string of input folder and outputfoler");
 }
