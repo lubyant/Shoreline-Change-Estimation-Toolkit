7fa25031103e1c8f304f31472009fbc78af90704
Lubyant <luboyuan1994@gmail.com>
2024-06-22 17:54:44 -0500

    wip

--- shorelinecalculator/app.py
+++ shorelinecalculator/app.py
@@ -1,2 +1,14 @@
-import cppext
-cppext.cppext("a", "b")
+from DL_running import process_img_folder, process_single_img
+from extract_merge_data import extract_zip
+
+
+
+class SCET:
+    def __init__(self, config) -> None:
+        pass
+
+    def method1(self, naip_zipfiles_folder):
+        extract_zip()
+
+    def method2(self):
+        pass
