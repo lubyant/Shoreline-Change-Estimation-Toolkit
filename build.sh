82955956cdeb38d84ba34a6fa66a41df7c4c5fda
lubyant <blu38@wisc.edu>
2024-06-26 16:28:40 -0500

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -y
+conda install boost gdal opencv pybind11 compilers pillow rasterio pyproj tqdm ftfy regex -c conda-forge -y
 pip install -U openmim
 mim install mmengine
 mim install "mmcv==2.0.0rc4"
