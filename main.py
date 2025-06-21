08e6311683b03f4eb583e7845b58f05ee3ba7558
lubyant <blu38@wisc.edu>
2025-06-20 20:49:38 -0500

    wip

--- main.py
+++
@@ -1,6 +0,0 @@
-from shorelinecalculator import Config, SCET
-config = Config("iter_100000.pth")
-scet = SCET(config)
-# scet.method1("LakeZero", "output")
-scet.method2("METHOD_SITE/4108604/4108604_2005.tif",
-             "output_single")
